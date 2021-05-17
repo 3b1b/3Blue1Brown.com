ee4ab7d729c18de5dc33a5f0cc4bea32244e7487
Vincent Rubinetti <vince.rubinetti@gmail.com>
2021-05-17 01:51:47 -0400

    rename toc to lesson nav

---
+++ js/lesson-nav.js
@@ -0,0 +1,16 @@
+// expand or collapse lesson topic table of contents
+const toggleLessonNav = (button) => {
+  const lessonNav = button.closest(".lesson_nav");
+  if (lessonNav.dataset.open === "true") {
+    lessonNav.dataset.open = false;
+    button.dataset.tooltip = "Open lesson list";
+    button.querySelector("i").className = "fas fa-bars";
+  } else {
+    lessonNav.dataset.open = true;
+    button.dataset.tooltip = "Close lesson list";
+    button.querySelector("i").className = "fas fa-times";
+  }
+  lessonNav
+    .querySelector("[data-active='true']")
+    .scrollIntoView({ block: "center" });
+};
