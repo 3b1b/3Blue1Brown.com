4797018a4bf48d2e007ce16d45aa8b9ca7bf058f
Kurt <KurtBruns@users.noreply.github.com>
2021-05-28 11:02:59 -0600

    Workflow (#47)
    * Setup git lfs file types and created test post.
    
    * Fixed/updated git LFS URLs
    
    * Testing modified file and fixing path
    
    * Changed order of git lfs url for example
    
    * Created test sync action
    
    * Fixed bad find replace to correct variable names
    
    * Added step and flag to download git lfs objects
    
    * Set acl to public, testing workflow
    
    * Updated bad flag, testing workflow
    
    * Testing sync workflow
    
    * Fix typo, testing sync workflow
    
    * Added test figure and updated URL paths for example post
    
    * Test delete file and new Ubuntu version
    
    * Added flag to delete unused files. Testing workflow action.
    
    * Updated example
    
    * Adds mug animation
    
    * Changed old workflow to be consistent
    
    * Testing workflow, dry run
    
    * Changed bucket variable name
    
    * Changed include patterns to lessons directory. Testing workflow, dry run
    
    * Modified test date page date to not be a future date. Testing workflow.
    
    * updated gitattributes
    
    * updated gitattributes, testing workflow
    
    * Testing workflow: delete asset
    
    * Testing workflow: delete asset
    
    * Added production config site parameters, testing branch environment variable
    
    * Added parameter to test workflow
    
    * Testing workflow with example shortcode
    
    * Fixed gitattributes paths
    
    * Testing workflow, updated staging paths
    
    * Test of git-lfs
    
    * Deleted file that should have been tracked with git LFS
    
    * Fixed bad path.
    
    * Changed action to sync bucket on push to main and to prep for vercel deploy
    
    * Added text and image to essence of calculus
    
    * Added netlify config file
    
    * Changed boolean to string
    
    * Pushing arbitrary change to test netlify
    
    * Testing netlify preview
    
    * Testing netlify preview
    
    * Added netlify file config to test CLI
    
    * Modified figure shortcode to be aware of page bundle resources.
    
    * Test push, added snippets, calculus lesson 1 assets
    
    Co-authored-by: Grant Sanderson <grant@3blue1brown.com>
    Co-authored-by: Josh Pullen <hello@joshuapullen.com>

---
+++ .github/actions/sync/entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if [ -z "$BUCKET" ]; then
+  echo -e 'BUCKET variable is not set. Quitting.'
+  exit 1
+fi
+
+if [ -z "$ACCESS_KEY" ]; then
+  echo -e 'ACCESS_KEY is not set. Quitting.'
+  exit 1
+fi
+
+if [ -z "$SECRET_KEY" ]; then
+  echo -e 'SECRET_KEY is not set. Quitting.'
+  exit 1
+fi
+
+if [ -z "$SOURCE_DIR" ]; then
+  echo -e 'SOURCE_DIR is not set. Quitting.'
+  exit 1
+fi
+
+if [ -z "$REGION" ]; then
+  export REGION="us-east-1"
+fi
+
+if [ -e "$HOME/.s3cfg" ]; then
+  warn '.s3cfg file already exists in home directory and will be overwritten'
+fi
+
+echo '[default]' > "$HOME/.s3cfg"
+echo "access_key=$ACCESS_KEY" >> "$HOME/.s3cfg"
+echo "secret_key=$SECRET_KEY" >> "$HOME/.s3cfg"
+echo "host_base = $REGION.linodeobjects.com" >> "$HOME/.s3cfg"
+echo "host_bucket = %(bucket)s.$REGION.linodeobjects.com" >> "$HOME/.s3cfg"
+echo "website_endpoint = http://%(bucket)s.website-$REGION.linodeobjects.com" >> "$HOME/.s3cfg"
+
+echo "Generated .s3cfg for key $ACCESS_KEY"
+
+# TODO: move to variable 
+echo 'lessons/**/*.jpg' > "$HOME/sync-patterns"
+echo 'lessons/**/*.png' >> "$HOME/sync-patterns"
+echo 'lessons/**/*.mp4' >> "$HOME/sync-patterns"
+echo 'lessons/**/*.mov' >> "$HOME/sync-patterns"
+
+echo "Generated sync include patterns"
+
+s3cmd --no-mime-magic --acl-public --delete-removed --delete-after --exclude "*" --include-from $HOME/sync-patterns sync $SOURCE_DIR s3://$BUCKET
+
+echo 'Removing .s3cfg credentials'
+rm "$HOME/.s3cfg"
