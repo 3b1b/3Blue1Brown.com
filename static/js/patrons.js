37eabc45fa219ec1bf73b616dfa57f9a2f2e246d
Vincent Rubinetti <vince.rubinetti@gmail.com>
2021-05-28 22:15:26 -0400

    add support button under patrons

--- static/js/patrons.js
+++ static/js/patrons.js
@@ -3,9 +3,11 @@ const togglePatrons = (button) => {
   const patrons = document.querySelector(".patrons");
   if (patrons.dataset.open === "true") {
     patrons.dataset.open = false;
-    button.querySelector("span").innerHTML = "See More";
+    button.querySelector(".clickable_icon i").className = "fas fa-angle-down";
+    button.querySelector(".clickable_text").innerHTML = "See More";
   } else {
     patrons.dataset.open = true;
-    button.querySelector("span").innerHTML = "See Less";
+    button.querySelector(".clickable_icon i").className = "fas fa-angle-up";
+    button.querySelector(".clickable_text").innerHTML = "See Less";
   }
 };
