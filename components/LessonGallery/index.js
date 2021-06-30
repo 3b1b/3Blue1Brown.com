3253517e361e78b5dfa4b74e97c45cc75ab99ff9
Grant Sanderson <grant@3blue1brown.com>
2021-06-30 11:54:11 -0700

    Site content additions (#157)
    * Adding absurd social icon tooltips
    
    * Keep header small on the home page
    
    * Tweaked social icons tooltips
    
    * Remove featured tab
    
    * Updated topic organization, descriptions, and thumbnails
    
    * Update about page
    
    * Update bottom of the home page
    
    * Fix spelling errors
    
    * For JsPrettier
    
    * Remove lessons page in favor of pointing to the home page
    
    * Link in about to manim
    
    * Add lessons header

--- components/LessonGallery/index.js
+++ components/LessonGallery/index.js
@@ -3,13 +3,12 @@ import Link from "next/link";
 import Center from "../Center";
 import Clickable from "../Clickable";
 import LessonCard from "../LessonCard";
-import featured from "../../data/featured.yaml";
 import topics from "../../data/topics.yaml";
 import { PageContext } from "../../pages/_app";
 import styles from "./index.module.scss";
 
 // gallery that shows all lessons in various ways with tabs. show by featured,
-// topic, or date
+// topic, or all
 const LessonGallery = ({ show = "topic" }) => {
   const { lessons } = useContext(PageContext);
   const [tab, setTab] = useState(show); // active tab
@@ -18,26 +17,16 @@ const LessonGallery = ({ show = "topic" }) => {
     <>
       <Center>
         <Clickable
-          text="Featured"
-          onClick={() => setTab("featured")}
-          active={tab === "featured"}
-        />
-        <Clickable
-          text="By Topic"
+          text="Topics"
           onClick={() => setTab("topic")}
           active={tab === "topic"}
         />
         <Clickable
-          text="By Date"
-          onClick={() => setTab("date")}
-          active={tab === "date"}
+          text="All"
+          onClick={() => setTab("all")}
+          active={tab === "all"}
         />
       </Center>
-      {tab === "featured" &&
-        featured
-          .map((slug) => lessons.find((lesson) => lesson.slug === slug))
-          .filter((lesson) => lesson)
-          .map((lesson, index) => <LessonCard key={index} id={lesson.slug} />)}
       {tab === "topic" && (
         <Center>
           {topics.map((topic, index) => (
@@ -45,7 +34,7 @@ const LessonGallery = ({ show = "topic" }) => {
           ))}
         </Center>
       )}
-      {tab === "date" &&
+      {tab === "all" &&
         lessons.map((lesson, index) => (
           <LessonCard key={index} id={lesson.slug} />
         ))}
