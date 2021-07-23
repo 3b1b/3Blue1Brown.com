1c9490d8cfdba1f97d59f2ed6142a0ac9849d5bf
Josh Pullen <JoshPullen27@gmail.com>
2021-07-23 15:23:31 -0400

    Improve & simplify podcast links

--- components/PodcastLinks/index.js
+++ components/PodcastLinks/index.js
@@ -1,19 +1,14 @@
 import Link from "next/link";
-import Clickable from "../Clickable";
+import Tooltip from "../Tooltip";
 import styles from "./index.module.scss";
-import PropTypes from "prop-types";
 
-PodcastLinks.propTypes = {
-  design: PropTypes.oneOf(["embedded", "full"]),
-};
-
-export default function PodcastLinks({ design = "embedded" }) {
+export default function PodcastLinks() {
   return (
-    <div className={styles.podcastLinks} data-design={design}>
+    <div className={styles.podcastLinks}>
       <Link href="/podcast">
         <a className={styles.coverLink}>
           <img
-            src="/images/podcast/3b1b-podcast.jpeg"
+            src="/images/podcast/3b1b-podcast.svg"
             alt="The 3b1b Podcast"
             className={styles.cover}
           />
@@ -22,71 +17,88 @@ export default function PodcastLinks({ design = "embedded" }) {
 
       <div className={styles.info}>
         <div className={styles.apps}>
-          <a href="https://www.youtube.com/playlist?list=PLfx0NKbQXfNBc5n8LOXYRTMzvTofAY8d6">
-            <img
-              src="/images/podcast/badges/youtube-badge.svg"
-              alt="Listen on YouTube"
-            />
-          </a>
-          <a href="https://podcasts.apple.com/us/podcast/the-3b1b-podcast/id1576951213">
-            <img
-              src="/images/podcast/badges/applepodcasts-badge.svg"
-              alt="Listen on Apple Podcasts"
-            />
-          </a>
-          <a href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy82MzZiNDgyMC9wb2RjYXN0L3Jzcw==">
-            <img
-              src="/images/podcast/badges/googlepodcasts-badge.svg"
-              alt="Listen on Google Podcasts"
-            />
-          </a>
-          <a href="https://open.spotify.com/show/74ZzyhJx8NL5OBmv2RWXnB">
-            <img
-              src="/images/podcast/badges/spotify-badge.svg"
-              alt="Listen on Spotify"
-            />
-          </a>
-          <a href="https://www.breaker.audio/the-3b1b-podcast">
-            <img
-              src="/images/podcast/badges/breaker-badge.svg"
-              alt="Listen on Breaker"
-            />
-          </a>
-          <a href="https://overcast.fm/itunes1576951213/the-3b1b-podcast">
-            <img
-              src="/images/podcast/badges/overcast-badge.svg"
-              alt="Listen on Overcast"
-            />
-          </a>
-          <a href="https://pca.st/5vw36tdt">
-            <img
-              src="/images/podcast/badges/pocketcasts-badge.svg"
-              alt="Listen on Pocket Casts"
-            />
-          </a>
-          <a href="https://radiopublic.com/the-3b1b-podcast-WogjkA">
-            <img
-              src="/images/podcast/badges/radiopublic-badge.svg"
-              alt="Listen on RadioPublic"
-            />
-          </a>
-          <a href="https://anchor.fm/s/636b4820/podcast/rss">
-            <img
-              src="/images/podcast/badges/rss-badge.svg"
-              alt="Subscribe via RSS"
-            />
-          </a>
+          <Tooltip content="YouTube">
+            <a href="https://www.youtube.com/playlist?list=PLfx0NKbQXfNBc5n8LOXYRTMzvTofAY8d6">
+              <img
+                className={styles.icon}
+                src="/images/podcast/youtube.png"
+                alt="Listen on YouTube"
+              />
+            </a>
+          </Tooltip>
+          <Tooltip content="Apple Podcasts">
+            <a href="https://podcasts.apple.com/us/podcast/the-3b1b-podcast/id1576951213">
+              <img
+                className={styles.icon}
+                src="/images/podcast/applepodcasts.png"
+                alt="Listen on Apple Podcasts"
+              />
+            </a>
+          </Tooltip>
+          <Tooltip content="Google Podcasts">
+            <a href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy82MzZiNDgyMC9wb2RjYXN0L3Jzcw==">
+              <img
+                className={styles.icon}
+                src="/images/podcast/googlepodcasts.png"
+                alt="Listen on Google Podcasts"
+              />
+            </a>
+          </Tooltip>
+          <Tooltip content="Spotify">
+            <a href="https://open.spotify.com/show/74ZzyhJx8NL5OBmv2RWXnB">
+              <img
+                className={styles.icon}
+                src="/images/podcast/spotify.png"
+                alt="Listen on Spotify"
+              />
+            </a>
+          </Tooltip>
+          <Tooltip content="Breaker">
+            <a href="https://www.breaker.audio/the-3b1b-podcast">
+              <img
+                className={styles.icon}
+                src="/images/podcast/breaker.png"
+                alt="Listen on Breaker"
+              />
+            </a>
+          </Tooltip>
+          <Tooltip content="Overcast">
+            <a href="https://overcast.fm/itunes1576951213/the-3b1b-podcast">
+              <img
+                className={styles.icon}
+                src="/images/podcast/overcast.png"
+                alt="Listen on Overcast"
+              />
+            </a>
+          </Tooltip>
+          <Tooltip content="Pocket Casts">
+            <a href="https://pca.st/5vw36tdt">
+              <img
+                className={styles.icon}
+                src="/images/podcast/pocketcasts.png"
+                alt="Listen on Pocket Casts"
+              />
+            </a>
+          </Tooltip>
+          <Tooltip content="RadioPublic">
+            <a href="https://radiopublic.com/the-3b1b-podcast-WogjkA">
+              <img
+                className={styles.icon}
+                src="/images/podcast/radiopublic.png"
+                alt="Listen on RadioPublic"
+              />
+            </a>
+          </Tooltip>
+          <Tooltip content="RSS">
+            <a href="https://anchor.fm/s/636b4820/podcast/rss">
+              <img
+                className={styles.icon}
+                src="/images/podcast/rss.png"
+                alt="Subscribe via RSS"
+              />
+            </a>
+          </Tooltip>
         </div>
-        {design === "embedded" && (
-          <div>
-            <Clickable
-              icon="fas fa-podcast"
-              link="/podcast"
-              text="Learn more..."
-              design="rounded"
-            />
-          </div>
-        )}
       </div>
     </div>
   );
