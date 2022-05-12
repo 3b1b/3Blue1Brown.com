f2fb93daab3b7999e5fbfe5fa431bd35a03c58a6
ellcrev <32151400+ellcrev@users.noreply.github.com>
2022-05-11 20:15:15 -0700

    Removed Sentry to prevent conflict with PWA

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 const withYAML = require("next-yaml");
 const withPWA = require("next-pwa");
-const { withSentryConfig } = require("@sentry/nextjs");
+const runtimeCaching = require("next-pwa/cache");
 
 let config = {
-  compiler: {
-    styledComponents: true,
-  },
+  future: { webpack5: true },
   images: {
     domains: [
       "3b1b-posts.us-east-1.linodeobjects.com",
@@ -48,23 +46,24 @@ config = withPWA({
     // the PWA will entirely fill the user's storage
     // with ALL the lesson data on a separate thread.
     publicExcludes: ["!*.mp4", "!content/**/*"],
+    runtimeCaching,
   },
 });
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-if (process.env.NODE_ENV === "production") {
-  config = withSentryConfig(config, {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
+// if (process.env.NODE_ENV === "production") {
+//   config = withSentryConfig(config, {
+//     // Additional config options for the Sentry Webpack plugin. Keep in mind that
+//     // the following options are set automatically, and overriding them is not
+//     // recommended:
+//     //   release, url, org, project, authToken, configFile, stripPrefix,
+//     //   urlPrefix, include, ignore
 
-    silent: true, // Suppresses all logs
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
-  });
-}
+//     silent: true, // Suppresses all logs
+//     // For all available options, see:
+//     // https://github.com/getsentry/sentry-webpack-plugin#options.
+//   });
+// }
 
 module.exports = config;
