eedcf1dc256d48d049595a3db10ab58bff85d28f
Grant Sanderson <grant@3blue1brown.com>
2024-01-24 12:41:15 -0600

    Add an initial component for gallery of past contributors

---
+++ components/AboutPageGallery/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from './index.module.scss';
+import teamMembers from '../../data/teamMemberData';
+
+const TeamMemberLinks = ({ links }) => {
+    return (
+        <div className={styles.links}>
+            {links.map((link, index) => (
+                <a key={index}
+                   href={link.url}
+                   target="_blank"
+                   rel="noopener noreferrer"
+                   className={styles.link}
+                   aria-label={link.label}>
+                    <i className={link.icon}></i>
+                </a>
+            ))}
+        </div>
+    );
+};
+
+const TeamMember = ({ name, role, imageSrc, links }) => {
+    return (
+        <div className={styles.teamMember}>
+            <img src={imageSrc} alt={name} className={styles.headshot} />
+            <h3 className={styles.name}>{name}</h3>
+            <p className={styles.role}>{role}</p>
+            <TeamMemberLinks links={links} />
+        </div>
+    );
+};
+
+const AboutPageGallery = () => {
+    return (
+        <div className={styles.gallery}>
+            {teamMembers.map((member) => (
+                <TeamMember
+                    key={member.name}
+                    name={member.name}
+                    role={member.role}
+                    imageSrc={member.imageSrc}
+                    links={member.links}
+                />
+            ))}
+        </div>
+    );
+};
+
+
+export { AboutPageGallery, TeamMember };
---
+++ data/teamMemberData.js
@@ -0,0 +1,128 @@
+const teamMembers = [
+  {
+    name: "Quinn Brodsky",
+    role: "Filming and research for the Barber pole videos",
+    imageSrc: "images/about/quinn-brodsky.jpeg",
+    links: [
+      {
+        icon: "fa-solid fa-youtube",
+        url: " www.youtube.com/@quinnphys",
+        label: "YouTube",
+      },
+      {
+        icon: "fab fa-spotify",
+        url: "https://open.spotify.com/artist/6tBc4zGfGwgS7yoKpfNqs3?si=UDeNq2QKQNO9jHVYySC_mw&nd=1&dlsi=355c8e85bedb47ff",
+        label: "On Bandcap",
+      },
+      {
+        icon: "fab fa-linkedin",
+        url: "https://www.linkedin.com/in/quinn-brodsky/",
+        label: "LinkedIn",
+      },
+    ],
+  },
+  {
+    name: "Kurt Bruns",
+    role: "Video artwork and written adaptation of the Calculus series",
+    imageSrc: "images/about/kurt-bruns.png",
+    links: [
+      {
+        icon: "fab fa-github",
+        url: "https://kurtbruns.github.io/",
+        label: "Github",
+      },
+    ],
+  },
+  {
+    name: "Vincent Rubinetti",
+    role: "Composing music for the videos and web development",
+    imageSrc: "images/contact/vince-bandcamp-portrait.jpeg",
+    links: [
+      {
+        icon: "fa-solid fa-globe",
+        url: "https://vincentrubinetti.com/",
+        label: "Vice's website",
+      },
+      {
+        icon: "fab fa-bandcamp",
+        url: "https://vincerubinetti.bandcamp.com/",
+        label: "On Bandcap",
+      },
+      {
+        icon: "fab fa-spotify",
+        url: "https://open.spotify.com/artist/2SRhEEt2tlDQWxzwfUo9Dl",
+        label: "Spotify",
+      },
+    ],
+  },
+  {
+    name: "James Schloss",
+    role: "Organization and community management for the Summers of Math Exposition",
+    imageSrc: "images/about/james-schloss.png",
+    links: [
+      {
+        icon: "fa-solid fa-globe",
+        url: "https://www.algorithm-archive.org/",
+        label: "Algorithm Archive",
+      },
+      {
+        icon: "fab fa-youtube",
+        url: "https://www.youtube.com/c/LeiosOS",
+        label: "LeiosOS",
+      },
+      {
+        icon: "fab fa-twitch",
+        url: "https://www.twitch.tv/simuleios",
+        label: "Simuleios",
+      },
+    ],
+  },
+  {
+    name: "Josh Pullen",
+    role: "2021 Intern, website interactives and written adaptations of videos",
+    imageSrc: "images/about/josh-pullen.png",
+    links: [
+      {
+        icon: "fa-solid fa-globe",
+        url: "https://www.joshuapullen.com/",
+        label: "Josh's website",
+      },
+    ],
+  },
+  {
+    name: "River Way",
+    role: "2021 Intern, website interactives and written adaptations of videos",
+    imageSrc: "images/about/river-way.png",
+    links: [
+      {
+        icon: "fa-solid fa-globe",
+        url: "https://riverway.li/",
+        label: "River's website",
+      },
+    ],
+  },
+  {
+    name: "Vivek Verma",
+    role: "2021 Intern, written adaptations of videos",
+    imageSrc: "images/about/vivek-verma.jpeg",
+    links: [
+      {
+        icon: "fab fa-youtube",
+        url: "https://youtube.com/vcubingx",
+        label: "River's website",
+      },
+      {
+        icon: "fab fa-twitter",
+        url: "https://twitter.com/vcubingx",
+        label: "River's website",
+      },
+      {
+        icon: "fab fa-github",
+        url: "https://github.com/vivek3141",
+        label: "River's website",
+      },
+    ],
+  },
+];
+
+export default teamMembers;
