9d281764c2ed61565a8783dac95c677aa8282a2d
Grant Sanderson <grant@3blue1brown.com>
2024-01-24 09:05:12 -0800

    Attempted fix for image links (#391)
    * Use path.join in transformSrc
    
    * Replace improper use of path.join with simpler fix
    
    * Handle dir="" case
    
    * Tweak the bug fix

--- util/transformSrc.js
+++ util/transformSrc.js
@@ -1,5 +1,10 @@
 import { bucket } from "../data/site.yaml";
 
+function ensureLeadingSlash(str) {
+    if (str && !str.startsWith("/")) return "/" + str;
+    return str;
+}
+
 // change provided srcs (png & mp4) to external bucket location for production.
 export const transformSrc = (src, dir = "") => {
   if (src.endsWith(".mp4")) {
@@ -10,10 +15,10 @@ export const transformSrc = (src, dir = "") => {
     return src;
   } else if (
     process.env.NODE_ENV === "production" &&
-    process.env.NEXT_PUBLIC_NETLIFY_CONTEXT === "production" && // Not a deploy preview
+    // process.env.NEXT_PUBLIC_NETLIFY_CONTEXT === "production" && // Not a deploy preview
     !src.endsWith("svg")
   ) {
-    return bucket + dir + src;
+    return bucket + ensureLeadingSlash(dir + src);
   } else {
     return dir + src;
   }
