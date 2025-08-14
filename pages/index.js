f01651df38f8f10fb65454217f2f80f6b2f607ea
Grant Sanderson <grant@3blue1brown.com>
2025-08-14 09:42:57 -0500

    Add url parameter to play a video

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,35 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import NormalLayout from "../layouts/NormalLayout";
 import { pageProps } from "../util/pages";
+import { useFeaturedVideo } from "../util/featuredVideoContext";
 
-export default NormalLayout;
+function HomePage(props) {
+  const router = useRouter();
+  const { playLesson } = useFeaturedVideo();
+  const { lessons = [] } = props;
+
+  useEffect(() => {
+    const { v } = router.query;
+    
+    if (v) {
+      // Find the lesson with the matching slug
+      const targetLesson = lessons.find(lesson => lesson.slug === v);
+      
+      if (targetLesson && targetLesson.video && targetLesson.video.trim() !== '') {
+        // Auto-play the lesson
+        playLesson(targetLesson);
+      } else if (targetLesson) {
+        console.warn(`Lesson "${v}" found but has no video`);
+      } else {
+        console.warn(`Lesson "${v}" not found`);
+      }
+    }
+  }, [router.query, lessons, playLesson]);
+
+  return <NormalLayout {...props} />;
+}
+
+export default HomePage;
 
 export const getStaticProps = async () => await pageProps("index");
