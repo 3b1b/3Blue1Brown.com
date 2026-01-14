5562148d350d9ce6d1e4d2849cf38368f57cd0c3
Grant Sanderson <grant@3blue1brown.com>
2026-01-14 14:35:42 -0600

    Update the layout for Talent partner pages

--- layouts/TalentLayout.js
+++ layouts/TalentLayout.js
@@ -1,6 +1,12 @@
 import { useContext } from "react";
-import NormalLayout from "./NormalLayout";
+import Link from "next/link";
+import Head from "../components/Head";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Footer from "../components/Footer";
 import PageContent from "../components/PageContent";
+import Anchors from "../components/Anchors";
+import Glow from "../components/Glow";
 import Center from "../components/Center";
 import Clickable from "../components/Clickable";
 import Jump from "../components/Jump";
@@ -23,35 +29,31 @@ const TalentLayout = () => {
   } = useContext(PageContext);
 
   return (
-    <NormalLayout>
-      <div>
-        <Clickable
-          link="/talent"
-          text="More companies"
-          icon="fas fa-arrow-left"
-        />
-      </div>
-
-      <Section width="narrow">
-        <div className={styles.header}>
-          <img src={logo} alt={`${title} logo`} className={styles.logo} />
-          <div className={styles["header-text"]}>
-            <h1 className={styles.title}>{title}</h1>
-            <p className={styles.description}>{description}</p>
+    <>
+      <Head />
+      <Header light={true} />
+      <Anchors />
+      <Glow />
+      <Main>
+      {/* Company Banner */}
+      <div className={styles.companyBanner}>
+        <img src={banner} alt="" className={styles.bannerImage} />
+        <div className={styles.topOverlay}></div>
+        <div className={styles.backLink}>
+          <Link href="/talent">
+            <i className="fas fa-arrow-left"></i> More companies
+          </Link>
+        </div>
+        <div className={styles.bannerOverlay}>
+          <div className={styles.bannerContent}>
+            <div className={styles.titleRow}>
+              <img src={logo} alt={`${title} logo`} className={styles.companyLogo} />
+              <h1 className={styles.companyName}>{title}</h1>
+            </div>
+            <p className={styles.companyDescription}>{description}</p>
           </div>
         </div>
-      </Section>
-
-      <Section width="narrow" dark image={banner}>
-        <p
-          className={styles.highlights}
-          dangerouslySetInnerHTML={{ __html: highlights }}
-        />
-      </Section>
-
-      <Section width="narrow">
-        <p className={styles.introduction}>{introduction}</p>
-      </Section>
+      </div>
 
       <Section width="narrow">
         <Center>
@@ -76,6 +78,10 @@ const TalentLayout = () => {
         </Center>
       </Section>
 
+
+      <Section width="narrow">
+        <p className={styles.introduction}>{introduction}</p>
+      </Section>
       <div className={styles.images}>
         {images.map((src, index) => (
           <a key={index} href={src} target="_blank">
@@ -89,7 +95,9 @@ const TalentLayout = () => {
       </div>
 
       <Jump />
-    </NormalLayout>
+      </Main>
+      <Footer />
+    </>
   );
 };
 
