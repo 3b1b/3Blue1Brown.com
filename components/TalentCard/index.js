ed2db151ca5eaac9a6e50d288d3cbaf04b4da106
Grant Sanderson <grant@3blue1brown.com>
2026-01-14 12:24:31 -0600

    Simplify TalentCards

--- components/TalentCard/index.js
+++ components/TalentCard/index.js
@@ -1,32 +1,21 @@
 import PropTypes from "prop-types";
-import { useContext } from "react";
-import { PageContext } from "../../pages/_app";
 import styles from "./index.module.scss";
 
 TalentCard.propTypes = {
+  logo: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  tagline: PropTypes.string.isRequired,
   slug: PropTypes.string.isRequired,
 };
 
-export default function TalentCard({ slug }) {
-  const { talentMeta } = useContext(PageContext);
-
-  // Find company by slug
-  const company = talentMeta?.find((c) => c.slug === slug);
-
-  if (!company) {
-    console.error(`Company not found: ${slug}`);
-    return null;
-  }
-
+export default function TalentCard({ logo, name, tagline, slug }) {
   return (
     <a className={styles.talent_card} href={`talent/${slug}`}>
       <div className={styles.image}>
-        <img className={styles.banner} src={company.banner} alt="" />
-        <div className={styles.vignette}></div>
-        <img className={styles.logo} src={company.logo} alt="" />
+        <img className={styles.logo} src={logo} alt={name} />
       </div>
-      <div className={styles.title}>{company.title}</div>
-      <div className={styles.text}>{company.description}</div>
+      <div className={styles.title}>{name}</div>
+      <div className={styles.text}>{tagline}</div>
     </a>
   );
 }
