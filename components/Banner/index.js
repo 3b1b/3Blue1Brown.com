1466b88f30931f0c7c23f88168972bc4ed98bbf3
Grant Sanderson <grant@3blue1brown.com>
2026-01-14 13:11:53 -0600

    Move banner assets, and use a lighter weight page header on the talent page

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -4,12 +4,20 @@ import styles from "./index.module.scss";
 Banner.propTypes = {
   src: PropTypes.string.isRequired,
   alt: PropTypes.string,
+  title: PropTypes.string,
+  tagline: PropTypes.string,
 };
 
-export default function Banner({ src, alt = "" }) {
+export default function Banner({ src, alt = "", title, tagline }) {
   return (
     <div className={styles.banner}>
       <img src={src} alt={alt} />
+      {(title || tagline) && (
+        <div className={styles.overlay}>
+          {title && <div className={styles.title}>{title}</div>}
+          {tagline && <div className={styles.tagline}>{tagline}</div>}
+        </div>
+      )}
     </div>
   );
 }
