d5bf72533e94ac52cd81b2e66a0563d69a138baa
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-02-03 13:42:15 -0500

    set up e2e tests with playwright

---
+++ playwright.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, devices } from "@playwright/test";
+
+const port = 31415;
+const url = `http://localhost:${port}`;
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  workers: "75%",
+  retries: 1,
+  reporter: [["html", { open: process.env.CI ? "never" : "always" }]],
+
+  // shared settings
+  use: {
+    trace: "on-first-retry",
+    baseURL: url,
+  },
+
+  projects: [
+    { name: "chromium", use: { ...devices["Desktop Chrome"] } },
+    // { name: "firefox", use: { ...devices["Desktop Firefox"] } },
+    // { name: "webkit", use: { ...devices["Desktop Safari"] } },
+  ],
+
+  webServer: {
+    command: `react-router dev --port ${port}`,
+    url,
+    reuseExistingServer: true,
+  },
+});
