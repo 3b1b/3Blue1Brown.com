acc3bced54ebcd50c58528a5403d5dcde2f5be3e
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-03-07 17:53:53 -0500

    bring over remaining lessons, comment out problematic interactives for now

--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,6 @@ export default defineConfig([
     ".react-router",
     "playwright-report",
     "test-results",
-    "app/pages/lessons/old/",
   ]),
 
   // https://github.com/mdx-js/eslint-mdx/issues/92
