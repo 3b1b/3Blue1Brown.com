fe47c2b816f1cd5342fd2c54c2a3700a09da5534
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-05-05 21:14:55 -0400

    getMonth inc 1

--- app/util/hooks.ts
+++ app/util/hooks.ts
@@ -223,9 +223,9 @@ export const useEgg = () => {
       const today = new Date();
       let shape = "";
       // pi day!
-      if (today.getMonth() === 2 && today.getDate() === 14) shape = "pi";
+      if (today.getMonth() + 1 === 3 && today.getDate() === 14) shape = "pi";
       // tau day!
-      if (today.getMonth() === 5 && today.getDate() === 28) shape = "tau";
+      if (today.getMonth() + 1 === 6 && today.getDate() === 28) shape = "tau";
       // shape = "pi";
       if (!shape) return;
       const width = window.innerWidth / 2;
