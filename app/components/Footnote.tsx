95f89f4727f6df19c511755b32999f21d717ed4f
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-05-08 17:17:25 -0400

    make ref callback consistent

--- app/components/Footnote.tsx
+++ app/components/Footnote.tsx
@@ -8,15 +8,15 @@ export default function Footnote({ href, children }: Props) {
   return (
     <Tooltip trigger={<button className="badge size-4">{children}</button>}>
       <div
-        ref={(div) => {
-          if (!div || !href) return;
+        ref={(element) => {
+          if (!element || !href) return;
           // get footnote content element
           const target = document.querySelector(href);
           if (!target) return;
           // clone footnote content
           const clone = target.cloneNode(true) as Element;
           clone.querySelector("[data-footnote-backref]")?.remove();
-          div.innerHTML = clone.innerHTML;
+          element.innerHTML = clone.innerHTML;
         }}
       />
     </Tooltip>
