1b201be8052e24a8651275c4da2274f34e3176ff
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-05-16 12:43:59 -0400

    comment egg

--- app/util/hooks.ts
+++ app/util/hooks.ts
@@ -234,7 +234,7 @@ export const useEgg = () => {
       let shape = "";
       if (today.getMonth() + 1 === 3 && today.getDate() === 14) shape = "pi";
       if (today.getMonth() + 1 === 6 && today.getDate() === 28) shape = "tau";
-      shape = "pi";
+      // shape = "pi";
       if (!shape) return;
       const width = window.innerWidth / 2;
       const height = window.innerHeight / 2;
