fc6cdb12f7fd0367098fdfbc81cb38cdaf1a2059
Grant Sanderson <grant@3blue1brown.com>
2026-06-27 13:34:32 -0500

    Move redirects to netlify.toml

--- app/pages/NotFound.tsx
+++ app/pages/NotFound.tsx
@@ -8,40 +8,9 @@ import Meta from "~/components/Meta";
 import PiCreature from "~/components/PiCreature";
 import StrokeType from "~/components/StrokeType";
 
-// redirects
+// redirects with regex capture groups (simple path redirects live in netlify.toml)
 const redirects: Record<string, string> = {
-  "/faq": "/about#faqs",
-  "/faqs": "/about#faqs",
-  "/contact": "/about#contact",
-  "/blog": "/extras#blog",
-  "/podcast": "/extras#podcast",
-  "/home": "/",
-  "/lessons": "/",
   "/topics/(.+)": "/?topic=$1",
-  "/video": "/",
-  "/live": "/",
-  "/store": "https://store.dftba.com/collections/3blue1brown",
-  "/plushie": "/store",
-  "/thanks": "/about#thanks",
-  "/recommendations": "/blog/recommendations",
-  "/some1-results": "/blog/some1-results",
-  "/poems": "/blog/poems",
-  "/videos": "https://www.youtube.com/3blue1brown",
-  "/support": "https://www.patreon.com/3blue1brown",
-  "/early-attention": "https://www.patreon.com/3blue1brown",
-  "/subscribe": "https://www.youtube.com/c/3blue1brown?sub_confirmation=1",
-  "/brilliant": "https://brilliant.org/3b1b",
-  "/quaternion-explorable": "https://eater.net/quaternions",
-  "/mail": "https://3blue1brown.substack.com/",
-  "/some": "https://some.3b1b.co",
-  "/some1": "https://some.3b1b.co",
-  "/recommended": "/?topic=best-of",
-  "/eola": "/?topic=linear-algebra",
-  "/neural-networks": "/?topic=neural-networks",
-  "/nn": "/?topic=neural-networks",
-  "/calculus": "/?topic=calculus",
-  "/eoc": "/?topic=calculus",
-  "/janestreet": "/talent/janestreet",
 };
 
 export const clientLoader = async ({ request }: Route.LoaderArgs) => {
